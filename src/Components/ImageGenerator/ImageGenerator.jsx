32b16496613d206b26e88ef19cc046636bfb1f16
Nagy Barnabas <nagybarnabas@Nagy-MacBook-Pro.local>
2025-03-30 22:24:51 +0200

    Making the app work

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -1,22 +1,62 @@
-import React from 'react'
+import React, {useRef, useState} from 'react'
 import './ImageGenerator.css'
 import def_image from '../Assets/image.png'
 
 
 const ImageGenerator = () => {
+
+  const [image_url, setImage_url] = useState("/");  
+  let inputRef = useRef(null);
+
+  const [loading,setLoading] = useState("false");
+
+
+  const ImageGenerator = async () => {
+    if (inputRef.current.value==="") {
+        return 0;
+    }
+    setLoading(true);
+    const response = await fetch(
+        "https://api.openai.com/v1/images/generations",
+        {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json",
+                Authorization:
+                "Bearer secretkey",
+                "User-Agent":"Chrome",
+            },
+            body:JSON.stringify({
+                prompt: `${inputRef.current.value}`,
+                n:1,
+                size:"512x512",
+            }),
+
+        }
+    );
+    let data = await response.json();
+    let data_array = data.data;
+    setImage_url(data_array[0].url);
+    setLoading(false);
+  }
+
   return (
     <div className='ai-image-generator'>
         <div className="header">
             AI IMAGE <span>generator</span>
         </div>
         <div className="img-loading">
-                <div className="image">
-                    <img src={def_image} alt="" />
-                </div>
+            <div className="image">
+                <img src={image_url==="/"?def_image:image_url} alt="" />
+            </div>
+            <div className="loading">
+                <div className={loading?"loading-bar":"loading-bar-full"}></div>
+                <div className={loading?"loading-text":"display-none"}>Loading....</div>
+            </div>
         </div>
         <div className="search-box">
-            <input type="text" className='search-input' placeholder='Describe what you want to see' />
-            <div className="generate-btn">Generate</div>
+            <input type="text" ref={inputRef} className='search-input' placeholder='Describe what you want to see' />
+            <div className="generate-btn"  onClick={()=>{ImageGenerator()}}>Generate</div>
         </div>
     </div>
   )
