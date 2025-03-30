088cd600d934fca6e9dd0ebbe2463113b0815e91
Nagy Barnabas <nagybarnabas@Nagy-MacBook-Pro.local>
2025-03-30 21:31:40 +0200

    Adding content and styling

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './ImageGenerator.css'
+import def_image from '../Assets/image.png'
+
 
 const ImageGenerator = () => {
   return (
-    <div>
-        
+    <div className='ai-image-generator'>
+        <div className="header">
+            AI IMAGE <span>generator</span>
+        </div>
+        <div className="img-loading">
+                <div className="image">
+                    <img src={def_image} alt="" />
+                </div>
+        </div>
+        <div className="search-box">
+            <input type="text" className='search-input' placeholder='Describe what you want to see' />
+            <div className="generate-btn">Generate</div>
+        </div>
     </div>
   )
 }
